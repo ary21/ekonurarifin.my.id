3287ca2715a6d2b8df2945d91227547132aea54b
Eko nur arifin <aryjamers.21@gmail.com>
2021-10-28 17:15:41 +0700

    feat: change desc

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>My name Eko Arifin, love explore and code to solve problem. happy coding</code>
         </p>
       </main>
 
