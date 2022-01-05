4ce2b4e3d686e3ff0a5369d2ec87920c74a83175
Eko nur arifin <aryjamers.21@gmail.com>
2022-01-05 09:11:32 +0700

    update content

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,56 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  GithubShareButton,
+  GithubIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from 'next-share';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Hallo Next.js Starter!</title>
+        <title>Eko Nur Arifin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Eko Nur Arifin" />
         <p className="description">
-          <code>My name Eko Arifin, love explore and code to solve problem. happy coding</code>
+          <code>love coding, enjoy explore and happy to solve problem.</code>
+        </p>
+        <p className="description">
+          Find me here :
+        </p>
+        <p className="description">
+          <GithubShareButton
+            url={'https://github.com/ary21/'} >
+            <GithubIcon size={32} round />
+          </GithubShareButton>
+          <LinkedinShareButton
+            url={'https://www.linkedin.com/in/eko-nur-arifin/'} >
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+          <FacebookShareButton
+            url={'https://www.facebook.com/eko.nurarifin21/'} >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <TwitterShareButton
+            url={'https://twitter.com/EkonurArifin85'} >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            url={'https://api.whatsapp.com/send?phone=6281365066430'} >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
         </p>
       </main>
 
